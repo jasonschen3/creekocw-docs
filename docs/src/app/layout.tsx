a13028d4d7fbe48e06bc35df3bb76af5f69cfcab
Jason Chen <jasonschen3@gmail.com.>
2025-03-18 22:18:19 +0100

    feat: add icon

--- docs/src/app/layout.tsx
+++ docs/src/app/layout.tsx
@@ -1,11 +1,20 @@
-import './global.css';
-import { RootProvider } from 'fumadocs-ui/provider';
-import { Inter } from 'next/font/google';
-import type { ReactNode } from 'react';
+import "./global.css";
+import { RootProvider } from "fumadocs-ui/provider";
+import { Inter } from "next/font/google";
+import type { ReactNode } from "react";
+import type { Metadata } from "next";
 
 const inter = Inter({
-  subsets: ['latin'],
+  subsets: ["latin"],
 });
+
+export const metadata: Metadata = {
+  title: "Creek OCW",
+  description: "Cherry Creek High School OpenCourseWare",
+  icons: {
+    icon: "https://ugakd4mkxv.ufs.sh/f/QRXW6mPDvNgcODJnurmfb52Q6NjpkM7THVhzLqitUSY9d4rf",
+  },
+};
 
 export default function Layout({ children }: { children: ReactNode }) {
   return (
